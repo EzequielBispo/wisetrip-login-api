03274e319a5391e24c5c72b2ed131f24bdb9171c
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2025-02-12 21:04:28 -0300

    WT-11 - Classe Service implementada.

---
+++ src/main/java/com/wisetrip/loginapi/service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.wisetrip.loginapi.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.wisetrip.loginapi.model.Usuario;
+import com.wisetrip.loginapi.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+  
+    @Autowired
+    private UsuarioRepository repository;
+
+    public Usuario getUsuario(int id) {
+        return repository.findById(id);
+    }
+
+    public Usuario setUsuario(Usuario usuario) {
+        return repository.save(cliente);
+    }
+
+    public Usuario deleteUsuario(int id) {
+        Usuario usuario = repository.findById(id);
+        repository.delete(usuario);
+        return usuario;
+        
+    }
+    public Usuario save(Usuario usuario) {
+        return repository.save(usuario);
+    }
+}
