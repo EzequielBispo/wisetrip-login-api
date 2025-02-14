236613f58a21b252b55d4b1ae2e5f02a3b91937f
Ezequiel Bispo de Jesus <ezequiel.bispo@kyndryl.com>
2025-02-14 20:27:25 -0300

    WT-11 - Adicionada definição da API com Swagger.

--- src/main/java/com/wisetrip/loginapi/LoginApiApplication.java
+++ src/main/java/com/wisetrip/loginapi/LoginApiApplication.java
@@ -2,7 +2,10 @@ package com.wisetrip.loginapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(title = "API de Usuários"))
 @SpringBootApplication
 public class LoginApiApplication {
 
